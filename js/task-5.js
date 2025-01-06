9934b57252eacff251cc4fe88efbd50bfc4bad87
wptraffadmin <wptraffadmin@proton.me>
2025-01-06 14:00:49 +0200

    HW 7

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+// Додаємо обробник події на кнопку
+button.addEventListener('click', () => {
+  const randomColor = getRandomHexColor(); // Генеруємо випадковий колір
+  document.body.style.backgroundColor = randomColor; // Задаємо колір фону
+  colorSpan.textContent = randomColor; // Встановлюємо колір в span
+});
