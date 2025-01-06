be2f3c8ce36790b100e7cc2f3540be5a0bf56e57
wptraffadmin <wptraffadmin@proton.me>
2025-01-06 13:34:13 +0200

    HW 7

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget.elements;
+
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (!emailValue || !passwordValue) {
+        alert('All form fields must be filled in');
+        return;
+    }
+
+    const formData = {
+        email: emailValue,
+        password: passwordValue,
+    };
+
+    console.log(formData);
+
+    loginForm.reset();
+});
